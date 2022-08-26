cd67f5fdb497999c4d0b2e3d2f9c89cb264ebc1d
Xavier Hernandez <xavier@kodeops.io>
2022-08-26 15:14:33 +0200

    Rename to organisational_unit

--- src/Media/Artifact.php
+++ src/Media/Artifact.php
@@ -6,19 +6,19 @@ use kodeops\Prado\PradoRequest;
 
 class Artifact
 {
-    protected $organisation_unit_alias;
+    protected $organisational_unit_alias;
     protected $artifact_alias;
 
-    public function __construct(string $organisation_unit_alias, string $artifact_alias)
+    public function __construct(string $organisational_unit_alias, string $artifact_alias = null)
     {
         $this->pradoRequest = new PradoRequest();
-        $this->organisation_unit_alias = $organisation_unit_alias;
+        $this->organisational_unit_alias = $organisational_unit_alias;
         $this->artifact_alias = $artifact_alias;
     }
 
     public function get(array $params)
     {
-        $params['organisation_unit'] = $this->organisation_unit_alias;
+        $params['organisational_unit'] = $this->organisational_unit_alias;
         return $this->pradoRequest->post("api/1/artifacts", $params);
     }
 
